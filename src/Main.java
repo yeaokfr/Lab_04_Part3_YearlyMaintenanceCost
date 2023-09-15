31f2f3f79aeabf7942d57685da70c993c5f5301f
yeaokfr <chimariyanishanth@gmail.com>
2023-09-15 09:27:47 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What are your home maintenance costs for Summer?");
+        int summerCosts = scan.nextInt();
+        System.out.println("What are your home maintenance costs for Fall?");
+        int fallCosts = scan.nextInt();
+        System.out.println("What are your home maintenance costs for Winter?");
+        int winterCosts = scan.nextInt();
+        System.out.println("What are your home maintenance costs for Spring?");
+        int springCosts = scan.nextInt();
+
+        int yearlyCost = summerCosts + fallCosts + winterCosts + springCosts;
+
+        System.out.println("Your yearly maintenance cost is $ " + yearlyCost);
+
     }
 }
